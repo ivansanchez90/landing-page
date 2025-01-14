d346a62702212068df446e485f013dd75580abdb
ivansanchez90 <sanchezoliva.ivan@gmail.com>
2025-01-14 01:55:47 -0300

    estilos navbar pantallas pequeñas

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Logo from '../../assets/logo.png'
 
 interface navbarLinksProps {
@@ -55,35 +56,104 @@ const navbarRedes: navbarRedesProps[] = [
   },
 ]
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen)
+  }
   return (
-    <nav className='flex justify-between items-center px-4 py-3 sm:px-12 sm:py-6'>
-      <div>
-        <img className='w-[100px]' src={Logo} alt='logo' />
+    <nav>
+      <div className='flex justify-between items-center px-4 py-3 sm:px-12 sm:py-6'>
+        <div>
+          <img className='w-[100px]' src={Logo} alt='logo' />
+        </div>
+        {/* Menú Hamburguesa */}
+
+        <button onClick={toggleMenu} className='text-white md:hidden'>
+          <svg
+            className='w-6 h-6'
+            fill='none'
+            stroke='currentColor'
+            viewBox='0 0 24 24'
+          >
+            {isOpen ? (
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth={2}
+                d='M6 18L18 6M6 6l12 12'
+              />
+            ) : (
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth={2}
+                d='M4 6h16M4 12h16M4 18h16'
+              />
+            )}
+          </svg>
+        </button>
+        {/*  */}
+        <div className='hidden md:block'>
+          <ul className='flex space-x-4 sm:space-x-8'>
+            {navbarLinks.map((link) => (
+              <li key={link.id}>
+                <a
+                  className='text-white text-sm sm:text-lg hover:text-sky-200 transition-transform hover:scale-110 transform inline-block duration-300'
+                  href={link.link}
+                >
+                  {link.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className='hidden md:block'>
+          <ul className='flex space-x-4'>
+            {navbarRedes.map((link) => (
+              <li key={link.id}>
+                <a
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className='inline-block transition-transform duration-300 transform hover:scale-125'
+                  href={link.link}
+                >
+                  {link.icon}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div>
-        <ul className='flex space-x-4 sm:space-x-8'>
+      {/* Movil */}
+      <div
+        className={`md:hidden absolute w-full bg-[#EB5A3C] transition-all duration-300 ${
+          isOpen ? 'opacity-100 visible' : 'opacity-0 invisible'
+        }`}
+      >
+        <ul className='flex flex-col px-4 py-2'>
           {navbarLinks.map((link) => (
-            <li key={link.id}>
+            <li className='py-2 text-center' key={link.id}>
               <a
-                className='text-white text-sm sm:text-lg hover:text-sky-200 transition-transform hover:scale-110 transform inline-block duration-300'
+                className='text-white hover:text-sky-200'
                 href={link.link}
+                onClick={() => setIsOpen(false)}
               >
                 {link.title}
               </a>
             </li>
           ))}
         </ul>
-      </div>
-
-      <div>
-        <ul className='flex space-x-4'>
+        <ul className='flex space-x-4 px-4 py-2 border-t justify-center'>
           {navbarRedes.map((link) => (
             <li key={link.id}>
               <a
                 target='_blank'
                 rel='noopener noreferrer'
-                className='inline-block transition-transform duration-300 transform hover:scale-125'
+                className='inline-block'
                 href={link.link}
+                onClick={() => setIsOpen(false)}
               >
                 {link.icon}
               </a>
